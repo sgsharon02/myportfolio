87026d6cb288757ee97ad6a1935b3e95f55797bc
sgsharon02 <sgsharon02@gmail.com>
2020-10-10 22:33:26 +0530

    chnages done

---
+++ script.js
@@ -0,0 +1,22 @@
+$(function () {
+    var roles = ["Web Developer", "Code Enthusiast","Student", "VITian"];
+    var count = 0;
+    var $typeSpan = $("#intro .content h2 span").typist({
+      text: roles[count]
+    });
+    setInterval(() => {
+      $typeSpan
+        .typistRemove(roles[count++ % roles.length].length)
+        .typistPause(2000)
+        .typistAdd(roles[count % roles.length]);
+    }, 5000);
+    $(".navbar").hide();
+    $(window).scroll(function () {
+      if ($(window).scrollTop() > 40) {
+        $(".navbar").slideDown(100);
+      } else {
+        $(".navbar").slideUp(100);
+      }
+    });
+  });
+  
